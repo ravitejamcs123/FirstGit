8224471144db7d488138d4233ca57fda693e9860
Machireddi <chinna-sai-ravi-teja.machireddi@capgemini.com>
2024-09-03 15:13:02 +0530

    thirdcommit

--- src/Gitdemo.java
+++ src/Gitdemo.java
@@ -4,6 +4,7 @@ public class Gitdemo {
 	public static void main(String[] args) {
 		System.out.println("welcome to git");
 		System.out.println("Thanks sts");
+		System.out.println("ok git");
 	}
 
 }
