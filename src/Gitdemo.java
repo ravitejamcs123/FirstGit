b524514ce495ffba73d28f23e2548a33961d9c9d
ravitejamcs123 <chinna-sai-ravi-teja.machireddi@capgemini.com>
2024-09-03 15:17:46 +0530

    fifthcommit

--- src/Gitdemo.java
+++ src/Gitdemo.java
@@ -5,7 +5,8 @@ public class Gitdemo {
 		System.out.println("welcome to git");
 		System.out.println("Thanks sts");
 		System.out.println("ok git");
-		System.out.println("firstbranch");
+		System.out.println("welcome firstbranch");
+		System.out.println("Thanks sts from firstbranch");
 	}
 
 }
